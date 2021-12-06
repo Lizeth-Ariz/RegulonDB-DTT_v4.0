e5c5eef17a243093b87836eb3ff1a25898f49ed9
Lizeth-Ariz <liz.arizmendi13@gmail.com>
2021-12-06 15:01:54 -0600

    Se agrego cover y formulario

--- src/dtt.jsx
+++ src/dtt.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Title from './components/cover/title'
+import { Tabs } from './components/tab/tabs';
+import Form from './components/regulondbData/form';
 import "./styleSheet_regulonDB.css"
 
+
+const tabsInfo = [
+    { id: "01", name: "RegulonDB-Data", disabled: false },
+    { id: "02", name: "User Data", disabled: false }
+  ];
+
+  const tabs = [
+    <div id="01">
+      <article>
+        <br />
+        <Form></Form>
+      </article>
+    </div>,
+    <div id="02">
+      <article>
+        <br />
+        
+      </article>
+    </div>
+  ];
 
 export default function DTT() {
     return (
         <div>
-            hola
+            <Title></Title>
+            <Tabs tabSelect={"01"} tabsInfo={tabsInfo} tabs={tabs} />
         </div>
     )
 }
