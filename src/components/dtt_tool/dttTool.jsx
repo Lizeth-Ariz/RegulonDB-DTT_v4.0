f9bc522f043b413d03bf2ff0da769681adc9132b
galarcon-ccg <galarcon@ccg.unam.mx>
2022-01-18 14:02:58 -0600

    update zoom

--- src/components/dtt_tool/dttTool.jsx
+++ src/components/dtt_tool/dttTool.jsx
@@ -12,19 +12,19 @@ const DttTool = ({
     const getTransformParameters = (element) => {
         const transform = element.style.transform;
         let scale = 1,
-          x = 0,
-          y = 0;
+            x = 0,
+            y = 0;
         if (transform.includes("scale"))
-          scale = parseFloat(transform.slice(transform.indexOf("scale") + 6));
+            scale = parseFloat(transform.slice(transform.indexOf("scale") + 6));
         if (transform.includes("translateX"))
-          x = parseInt(transform.slice(transform.indexOf("translateX") + 11));
+            x = parseInt(transform.slice(transform.indexOf("translateX") + 11));
         if (transform.includes("translateY"))
-          y = parseInt(transform.slice(transform.indexOf("translateY") + 11));
+            y = parseInt(transform.slice(transform.indexOf("translateY") + 11));
         return { scale, x, y };
-      };
-    
-      const getTransformString = (scale, x, y) =>
-  "scale(" + scale + ") " + "translateX(" + x + "%) translateY(" + y + "%)";
+    };
+
+    const getTransformString = (scale, x, y) =>
+        "scale(" + scale + ")  translateX(" + x + "%) translateY(" + y + "%)";
 
 
     const _onZoom = (direction) => {
@@ -33,7 +33,7 @@ const DttTool = ({
             const { scale, x, y } = getTransformParameters(svg);
             let dScale = 0.1;
             if (direction === "out") dScale *= -1;
-            if (scale == 0.1 && direction === "out") dScale = 0;
+            if (scale === 0.1 && direction === "out") dScale = 0;
             svg.style.transform = getTransformString(scale + dScale, x, y);
         }
     };
